e98ce71cd8efa1d41149565960178da3e1e2f640
Sergio Hidalgo-Espinoza <barcecheo2319@gmail.com>
2022-03-02 05:26:49 -0500

    main

--- src/main/java/com/example/etl/ETLApp.java
+++ src/main/java/com/example/etl/ETLApp.java
@@ -1,51 +1,87 @@
 package com.example.etl;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import javax.swing.JOptionPane;
+import com.example.etl.entity.Salary;
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 public class ETLApp {
 
-  public Connection conectar() {
-    Connection con = null;
-    try {
-      Class.forName("org.h2.Driver");
-      con = DriverManager.getConnection( "jdbc:h2:.\\src\\main\\resources\\default", "sa", "");
-      System.out.println("En linea");
-    } catch (Exception ex) {
-      System.out.println("Error " + ex);
-    }
-    return con;
-  }
-
-  public ResultSet consultar() {
-    Connection con = conectar();
-    ResultSet rs = null;
-    try {
-      PreparedStatement ps = con.prepareStatement("SELECT * FROM \"AllstarFull\"");
-      rs = ps.executeQuery();
-
-    } catch (Exception ex) {
-      System.err.println("Error " + ex);
-    }
-
-    return rs;
-  }
-
   public static void main(String[] args) {
-    ETLApp con = new ETLApp();
-    ResultSet rs = con.consultar();
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    EntityTransaction transaction = entityManager.getTransaction();
 
     try {
-      while (rs.next()) {
-        JOptionPane.showMessageDialog(null,
-            rs.getString(1) + ", " + rs.getString(2) + ", " + rs.getString(3));
+      transaction.begin();
+
+      TypedQuery<Salary> query = entityManager.createNamedQuery("Salary.getFieldingSalaries",
+          Salary.class);
+      TypedQuery<Salary> query2 = entityManager.createNamedQuery("Salary.getPitchingSalaries",
+          Salary.class);
+
+      Map<Integer, List<Double>> salaries = new TreeMap<>();
+
+      String[] title = {"Year", "Fielding"};
+      String name = "AverageSalaries.csv";
+
+      CSVWriter writer = new CSVWriter(new FileWriter(name));
+      writer.writeNext(title);
+
+      for (Map.Entry<Integer, List<Integer>> list : mapSalaries(query.getResultList()).entrySet()) {
+        salaries.put(list.getKey(), new ArrayList<>(Collections.singleton(
+            list.getValue().stream().mapToDouble(value -> value).average().orElse(0.0))));
       }
-    } catch (Exception ex) {
-      System.out.println(ex);
+
+      for (Map.Entry<Integer, List<Integer>> list : mapSalaries(
+          query2.getResultList()).entrySet()) {
+        List<Double> newList = new ArrayList<>(salaries.get(list.getKey()));
+        newList.add(list.getValue().stream().mapToDouble(value -> value).average().orElse(0.0));
+        salaries.put(list.getKey(), newList);
+
+        writer.writeNext(new String[]{list.getKey().toString(), Arrays.toString(newList.toArray())});
+      }
+
+      writer.close();
+      System.out.println(salaries);
+
+      transaction.commit();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (transaction.isActive()) {
+        transaction.rollback();
+      }
+
+      entityManager.close();
+      entityManagerFactory.close();
+    }
+  }
+
+  public static Map<Integer, List<Integer>> mapSalaries(List<Salary> salaries) {
+    Map<Integer, List<Integer>> result = new TreeMap<>();
+    for (Salary s : salaries) {
+      if (result.containsKey(s.getYearID())) {
+        List<Integer> newList = new ArrayList<>(result.get(s.getYearID()));
+        newList.add(s.getPlayerSalary());
+
+        result.put(s.getYearID(), newList);
+      } else {
+        result.put(s.getYearID(), List.of(s.getPlayerSalary()));
+      }
     }
 
+    return result;
   }
 }
